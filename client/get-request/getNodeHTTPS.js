56dc5389aa63de02ea7340b41d705deeb757a369
illialaz <illia.laziuk2@gmail.com>
2020-07-27 16:39:24 +0300

    Client: - Try GET and POST requests using different ways to interact with server
    Server:
    - Tripcost program to count and store data from your trips

---
+++ client/get-request/getNodeHTTPS.js
@@ -0,0 +1,23 @@
+const http = require('https')
+
+const options = {
+  method: 'GET',
+  hostname: 'dog.ceo',
+  path: '/api/breeds/list/all',
+}
+
+const req = http.request(options, res => {
+  const chunks = []
+
+  res.on('data', chunk => {
+    chunks.push(chunk)
+  })
+
+  res.on('end', () => {
+    const body = Buffer.concat(chunks)
+    const data = JSON.parse(body.toString())
+    for (const breed in data.message) {
+      console.log(breed)
+    }
+  })
+})
---
+++ client/get-request/getXML.js
@@ -0,0 +1,13 @@
+const xhr = new XMLHttpRequest()
+xhr.addEventListener('readystatechange', function() {
+  if (this.readyState === this.DONE) {
+    const breeds = JSON.parse(this.responseText).message
+    for (const breed in breeds) {
+      console.log(breed)
+    }
+  }
+})
+
+xhr.open('GET', 'https://dog.ceo/api/breeds/list/all')
+
+xhr.send()
